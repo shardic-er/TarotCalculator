bffadc777035fda88925b8f38636ae77f97ec23c
Shardic <eli.ronai@gmail.com>
2024-07-22 18:37:20 -0500

    configs configed to be configed

--- src/config.tsx
+++ src/config.tsx
@@ -18,4 +18,4 @@ export const affinityColors: Record<Affinity, string> = {
 
 export const defaultShowImpactPointsOnTooltip: boolean = false
 export const defaultHideImpressionIconsUntilSelected = true
-export const defaultConfigsLocked = true
+export const defaultConfigsLocked = false
